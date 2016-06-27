762771cf055f15dd22129a719177cb8d802d72ff
Vivi <rocha.vivi@gmail.com>
2016-06-27 19:39:27 -0300

    Atributos do livro

--- src/main/java/br/com/primeiroJava/model/Livro.java
+++ src/main/java/br/com/primeiroJava/model/Livro.java
@@ -4,4 +4,12 @@ package br.com.primeiroJava.model;
  * Created by Kleber on 27/06/2016.
  */
 public class Livro {
-}
+
+    public String titulo;
+    public String isbn;
+    public String lancamento;
+
+
+
+    }
+
